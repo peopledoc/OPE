a08a6e9b6d3c77deca17898524079127675fa77a
Sophie S <sophie_surmont@ultimatesoftware.com>
2020-06-12 10:56:25 +0200

    Make the indicator stack, add women and men in bar

--- js/indicators/type_of_contracts.js
+++ js/indicators/type_of_contracts.js
@@ -1,29 +1,52 @@
 function display_type_of_contract(lines, elementId) {
-    let contracts = {}
+    let contracts_h = {}
+    let contracts_f = {}
     for (let i = 0; i < lines.length; i++) {
         let contract = String(lines[i][8]);
-        if (contract in contracts)
+
+        if (!(contract in contracts_f))
         {
-            contracts[contract] += 1;
+            contracts_f[contract] = 0;
+            contracts_h[contract] = 0;
+        }
+        if (lines[i][1] == 'F')
+        {
+            contracts_f[contract] += 1;
         }
         else
         {
-            contracts[contract] = 0;
+            contracts_h[contract] += 1;
         }
     }
 
-    let contracts_label = Object.keys(contracts);
-    let contracts_value = Object.values(contracts);
+    let dataset = [
+    { 
+        label : 'Femmes',
+        data : Object.values(contracts_f),
+        backgroundColor: '#0D533D'
+    },
+    {
+        label : 'Hommes',
+        data : Object.values(contracts_h),
+        backgroundColor: '#58BBA6'
+    }
+    ]
+
+    let contracts_label = Object.keys(contracts_f);
 
     const ctx = document.getElementById(elementId).getContext('2d');
     new Chart(ctx, {
         type: 'horizontalBar',
         data: {
             labels: contracts_label,
-            datasets: [{
-                data: contracts_value,
-                backgroundColor: ['#58BBA6', '#0D533D', '#11533D']
-            }]
+            datasets: dataset
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    stacked: true
+                }]
+            }
         }
     });
 }
